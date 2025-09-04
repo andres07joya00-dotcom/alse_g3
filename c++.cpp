062d327f3cd178e8ad8c39552a0cad0aaf583eb5
joyita <andres07joya00@gmail.com>
2025-09-04 15:23:12 -0500

    cin>>

--- c++.cpp
+++ c++.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 float num=3.14;
 void func(){
-    std::cout<<"global variable: "<<num<<std::endl;
+    std::cout<<"global variable: "<< num <<std::endl;
+    std::cout<<"global variable: "<< &num <<std::endl;
+    std::cout<<"global variable: "<< sizeof(num) <<std::endl;
     //std::cout<<"global variable: "<<num<<std::endl;
 
 
@@ -9,8 +11,11 @@ void func(){
 
 int main(){
     int a=1;
+    int x,y;
     std::cout<<"global variable: "<<num<<std::endl;
     std::cout<<"main func variable: "<<a<<std::endl;
     func();
+
+    std::cin >> x >>y;
     return 0;
 }
