f1f755311a5b19293534957756c2e562d4110af2
joyita <andres07joya00@gmail.com>
2025-09-03 19:25:19 -0500

    concatenar

--- c++.cpp
+++ c++.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 float num=3.14;
 void func(){
-std::cout<<"global variable: "<<num<<std::endl;
+    std::cout<<"global variable: "<<num<<std::endl;
+    //std::cout<<"global variable: "<<num<<std::endl;
 
 
 }
 
 int main(){
     int a=1;
+    std::cout<<"global variable: "<<num<<std::endl;
+    std::cout<<"main func variable: "<<a<<std::endl;
+    func();
+    return 0;
 }
